deeffc6ca125c70b8535e2f00f6fefe7bd2ba755
jkassman <jkassman@nd.edu>
2022-08-28 19:25:57 -0400

    Clean up deploy.py, add arg to skip updates (#2)

--- deploy.py
+++ deploy.py
@@ -5,20 +5,32 @@ import zipfile
 import time
 import os
 
+# avoid:
+# botocore.errorfactory.ResourceConflictException:
+#   An error occurred (ResourceConflictException) when calling the
+#   UpdateFunctionCode operation: The operation cannot be performed at this time.
+#   An update is in progress for resource: <arn_of_resource>
+BLIND_CONFIG_WAIT_TIME = 1.5
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--force_updates', action="store_true")
+args = parser.parse_args()
+
+
 rest_api_id = "zllbrt4p87"
 api_name = "killerfrog-api-demo"
-lambda_name = "NOTUSED"
 
 api_gateway_json = {
-  "swagger": "2.0",
+    "swagger": "2.0",
     "info": {
-      "version": "2018-07-21T19:28:58Z", # TODO Investigate
-      "title": api_name
+        "version": "2018-07-21T19:28:58Z", # TODO Investigate
+        "title": api_name
     },
     "host": f"{rest_api_id}.execute-api.us-east-2.amazonaws.com",
     "basePath": "/dev",
     "schemes": [
-      "https"
+        "https"
     ],
 }
 
@@ -34,68 +46,52 @@ All python files should start with:
 files = os.listdir()
 paths = []
 for filename in files:
-  if os.path.isfile(filename) and filename.endswith(".py") and filename != "deploy.py":
-    line_top = ""
-    with open(filename) as f:
-      for i, line in enumerate(f):
-        line_top += line
-        if i >= 2:
-          break
-    path = {"file": filename}
-    for i, line in enumerate(line_top.split("\n")):
-      if i >= 3:
-        break
-      split_line = line.split()
-      assert split_line[0] == "#", invalid_file_top_message % (filename, line_top)
-      if i == 0:
-        assert split_line[1] == "API_GATEWAY", invalid_file_top_message % (filename, line_top)
-        path["api_gateway"] = split_line[2]
-      if i == 1:
-        path["method"] = split_line[1]
-        path["path"] = split_line[2]
-      if i == 2:
-        assert split_line[1] == "LAMBDA", invalid_file_top_message % (filename, line_top)
-        path["lambda"] = split_line[2]
-    paths.append(path)
+    if os.path.isfile(filename) and filename.endswith(".py") and filename != "deploy.py":
+        line_top = ""
+        with open(filename) as f:
+            for i, line in enumerate(f):
+                line_top += line
+                if i >= 2:
+                    break
+        path = {"file": filename}
+        for i, line in enumerate(line_top.split("\n")):
+            if i >= 3:
+                break
+            split_line = line.split()
+            assert split_line[0] == "#", invalid_file_top_message % (filename, line_top)
+            if i == 0:
+                assert split_line[1] == "API_GATEWAY", invalid_file_top_message % (filename, line_top)
+                path["api_gateway"] = split_line[2]
+            elif i == 1:
+                path["method"] = split_line[1]
+                path["path"] = split_line[2]
+            if i == 2:
+                assert split_line[1] == "LAMBDA", invalid_file_top_message % (filename, line_top)
+                path["lambda"] = split_line[2]
+        paths.append(path)
 
 print(json.dumps(paths, indent=4))
-'''
-paths = [
-  {"path": "/get-value",
-  "method": "GET",
-  "lambda": "killerfrog-api-demo_get_value",
-  "produces": "application/json",
-  "file": "api_demo_get_value.py",
-  },
-  {"path": "/store-value",
-  "method": "POST",
-  "lambda": "killerfrog-api-demo_store_value",
-  "produces": "text/plain", # TOCONSIDER remove
-  "file": "api_demo_store_value.py",
-  }
-]
-'''
 
 gateway_paths = {}
 for path in paths:
-  method = path["method"]
-  gateway_paths[path["path"]] = {
-      method.lower(): {
-        "produces": [
-          "application/json"
-        ],
-        "x-amazon-apigateway-integration": {
-          "uri": (
-            f"arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/"
-            f"arn:aws:lambda:us-east-2:539164448633:function:"
-            f"{path['lambda']}/invocations"
-          ),
-          "passthroughBehavior": "when_no_match",
-          "httpMethod": "POST", # Note, POST required even for get
-          "contentHandling": "CONVERT_TO_TEXT",
-          "type": "aws_proxy"
+    method = path["method"]
+    gateway_paths[path["path"]] = {
+        method.lower(): {
+            "produces": [
+                "application/json"
+            ],
+            "x-amazon-apigateway-integration": {
+                "uri": (
+                    f"arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/"
+                    f"arn:aws:lambda:us-east-2:539164448633:function:"
+                    f"{path['lambda']}/invocations"
+                ),
+                "passthroughBehavior": "when_no_match",
+                "httpMethod": "POST", # Note, POST required even for get
+                "contentHandling": "CONVERT_TO_TEXT",
+                "type": "aws_proxy"
+            }
         }
-      }
     }
 api_gateway_json["paths"] = gateway_paths
 
@@ -119,16 +115,10 @@ deploy_api_gateway(api_gateway_json)
 
 def create_lambda(client, lambda_name, lambda_file, api_source_arn):
     iam_role = "midway_lambda" # TODO customize this
-    #zipped_bytes = bytes()
-    zipped_bytes = io.BytesIO() #zipped_bytes, "w")
+    zipped_bytes = io.BytesIO()
     with zipfile.ZipFile(zipped_bytes, "w") as z:
         z.write(lambda_file)
-        #z.writestr("asfsfd", "adfsdfsadf")
-        #print(zipped_bytes.read())
 
-    #zipped_bytes.flush()
-    #print(zipped_bytes.getvalue())
-    #exit(0)
     config = dict(
         FunctionName=lambda_name,
         Role=f"arn:aws:iam::539164448633:role/{iam_role}",
@@ -144,40 +134,42 @@ def create_lambda(client, lambda_name, lambda_file, api_source_arn):
         **config,
       )
     except client.exceptions.ResourceConflictException:
-      client.update_function_configuration(**config)
-      time.sleep(1) # avoid botocore.errorfactory.ResourceConflictException: An error occurred (ResourceConflictException) when calling the CreateFunction operation: Function already exist: killerfrog-api-demo_store_value
-      client.update_function_code(
-        FunctionName=lambda_name,
-        ZipFile=zipped_bytes.getvalue()
-      )
-    statement_id = f'{lambda_name}-policy'
-    client.remove_permission(
-        FunctionName=lambda_name,
-        StatementId=statement_id,
-        #Qualifier='string',
-        #RevisionId='string'
-    )
-    time.sleep(1)
-    client.add_permission(
-        FunctionName=lambda_name,
-        StatementId=statement_id,
-        Action='lambda:InvokeFunction',
-        Principal='apigateway.amazonaws.com',
-        SourceArn=api_source_arn
-        #SourceAccount='string',
-        #EventSourceToken='string',
-        #Qualifier='string',
-        #RevisionId='string',
-        #PrincipalOrgID='string',
-        #FunctionUrlAuthType='NONE'|'AWS_IAM'
-    )
+        if args.force_updates:
+            client.update_function_configuration(**config)
+            time.sleep(BLIND_CONFIG_WAIT_TIME)
+        client.update_function_code(
+            FunctionName=lambda_name,
+            ZipFile=zipped_bytes.getvalue()
+        )
+    if args.force_updates:
+        statement_id = f'{lambda_name}-policy'
+        client.remove_permission(
+            FunctionName=lambda_name,
+            StatementId=statement_id,
+            #Qualifier='string',
+            #RevisionId='string'
+        )
+        time.sleep(BLIND_CONFIG_WAIT_TIME)
+        client.add_permission(
+            FunctionName=lambda_name,
+            StatementId=statement_id,
+            Action='lambda:InvokeFunction',
+            Principal='apigateway.amazonaws.com',
+            SourceArn=api_source_arn
+            #SourceAccount='string',
+            #EventSourceToken='string',
+            #Qualifier='string',
+            #RevisionId='string',
+            #PrincipalOrgID='string',
+            #FunctionUrlAuthType='NONE'|'AWS_IAM'
+        )
 
 
 lambda_client = boto3.client("lambda")
 
 for path in paths:
-  api_source_arn =  (
-    f"arn:aws:execute-api:us-east-2:539164448633:{rest_api_id}"
-    f"/*/{path['method'].upper()}{path['path']}"
-  )
-  create_lambda(lambda_client, path["lambda"], path["file"], api_source_arn)
+    api_source_arn =  (
+        f"arn:aws:execute-api:us-east-2:539164448633:{rest_api_id}"
+        f"/*/{path['method'].upper()}{path['path']}"
+    )
+    create_lambda(lambda_client, path["lambda"], path["file"], api_source_arn)
